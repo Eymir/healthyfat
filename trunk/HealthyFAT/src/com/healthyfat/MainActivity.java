8ae4baf38708e92f42a4b5542ff5ff79c4e2f059
xiangfight <xiangfight@gmail.com>
2013-12-29 02:03:30 +0000

    Facebook Login and Logout implemented (Test codes)

--- trunk/HealthyFAT/src/com/healthyfat/MainActivity.java
+++ trunk/HealthyFAT/src/com/healthyfat/MainActivity.java
@@ -4,6 +4,7 @@ import com.healthyfat.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.widget.TextView; //a
 import android.content.Intent; //a
@@ -11,35 +12,52 @@ import android.content.Intent; //a
 import com.facebook.*; //a
 import com.facebook.model.*; //a
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+	
+	private MainFragment mainFragment;
+	private static final String TAG = "MainFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+            .beginTransaction()
+            .add(android.R.id.content, mainFragment)
+            .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+            .findFragmentById(android.R.id.content);
+        }
+        
+        
         //start facebook login
-        Session.openActiveSession(this, true, new Session.StatusCallback() {
+        //Session.openActiveSession(this, true, new Session.StatusCallback() {
         	
         	//callback when session changes state
-        	@Override
-            public void call(Session session, SessionState state, Exception exception) {
-        		if (session.isOpened()) {
+        	//@Override
+            //public void call(Session session, SessionState state, Exception exception) {
+        		//if (session.isOpened()) {
         			// make request to the /me API
-        			Request.newMeRequest(session, new Request.GraphUserCallback() {
+        			//Request.newMeRequest(session, new Request.GraphUserCallback() {
         	        	  
         	        	// callback after Graph API response with user object
-        	              @Override
-        	              public void onCompleted(GraphUser user, Response response) {
-        	                if (user != null) {
-        	                  TextView welcome = (TextView) findViewById(R.id.welcome);
-        	                  welcome.setText("Hello " + user.getName() + "!");
-        	                }
-        	              }
-        	        }).executeAsync();
-        		}
-            }
-        });
+        	              //@Override
+        	              //public void onCompleted(GraphUser user, Response response) {
+        	                //if (user != null) {
+        	                  //TextView welcome = (TextView) findViewById(R.id.welcome);
+        	                  //welcome.setText("Hello " + user.getName() + "!");
+        	                //}
+        	              //}
+        	        //}).executeAsync();
+        		//}
+            //}
+        //});
     }
 
 
@@ -50,10 +68,10 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) { //a
-      super.onActivityResult(requestCode, resultCode, data);
-      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-    }
+   // @Override
+    //public void onActivityResult(int requestCode, int resultCode, Intent data) { //a
+     // super.onActivityResult(requestCode, resultCode, data);
+      //Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+   // }
     
 }
