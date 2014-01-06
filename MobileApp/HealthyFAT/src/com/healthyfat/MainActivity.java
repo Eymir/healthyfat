32cebf4f255dfd7b27727290fcb5f792b49ad933
xiangfight@gmail.com <xiangfight@gmail.com@161c193b-5751-c41e-5782-dda1ea88aff3>
2014-01-06 13:57:24 +0000

    Added LoginActivity.
    Nav Diag completed -> When reached upon MainActivity, the system will check if the user is first time using the application:
    If yes: Nav to PreLoginActivity
    If No: Stay
    
    At PreLoginActivity, click one button and nav to LoginActivity.
    LoginActivity can nav back to PreLoginActivity though action bar

--- MobileApp/HealthyFAT/src/com/healthyfat/MainActivity.java
+++ MobileApp/HealthyFAT/src/com/healthyfat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.healthyfat;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
--- MobileApp/HealthyFAT/src/com/healthyfat/PreLoginActivity.java
+++ MobileApp/HealthyFAT/src/com/healthyfat/PreLoginActivity.java
@@ -2,14 +2,22 @@ package com.healthyfat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class PreLoginActivity extends Activity {
+public class PreLoginActivity extends Activity implements OnClickListener {
 
+	Button b;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pre_login);
+		b = (Button) findViewById(R.id.button1);
+		b.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +25,12 @@ public class PreLoginActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.pre_login, menu);
 		return true;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		Intent i = new Intent(this,LoginActivity.class);
+		startActivity(i);
 	}
 
 }
