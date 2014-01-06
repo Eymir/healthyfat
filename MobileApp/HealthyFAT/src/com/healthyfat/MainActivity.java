ce3768668780d89150a8c18a6d3a49d452d67946
xiangfight <xiangfight@gmail.com>
2014-01-06 07:22:56 +0000

    Commit to fix Previous Commit Error

--- MobileApp/HealthyFAT/src/com/healthyfat/MainActivity.java
+++ MobileApp/HealthyFAT/src/com/healthyfat/MainActivity.java
@@ -2,14 +2,19 @@ package com.healthyfat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	SharedPreferences prefs = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		prefs = getSharedPreferences("com.healthyfat", MODE_PRIVATE);
 	}
 
 	@Override
@@ -17,6 +22,24 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		/*
+		 * If "firstrun" is true, navigate to PreLogin Screen
+		 * commit the prefs to take note the "firstrun" is now false
+		 */
+		
+		if (prefs.getBoolean("firstrun", true)) {
+			
+			Intent i = new Intent (this, PreLoginActivity.class);
+			startActivity(i);
+			
+			prefs.edit().putBoolean("firstrun", false).commit();
+		}
 	}
 
 }
