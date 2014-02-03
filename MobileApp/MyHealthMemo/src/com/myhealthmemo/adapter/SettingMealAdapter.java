293dff8d327d2bf8667d91e9821e8af808147eab
limyihan@gmail.com <limyihan@gmail.com@161c193b-5751-c41e-5782-dda1ea88aff3>
2014-02-03 06:00:28 +0000

    fix SettingMealAdapter

--- MobileApp/MyHealthMemo/src/com/myhealthmemo/adapter/SettingMealAdapter.java
+++ MobileApp/MyHealthMemo/src/com/myhealthmemo/adapter/SettingMealAdapter.java
@@ -1,24 +1,24 @@
-/*package com.myhealthmemo.adapter;
+package com.myhealthmemo.adapter;
 
 import java.util.ArrayList;
-
-import com.myhealthmemo.model.MealsItem;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.myhealthmemo.R;;
+import com.myhealthmemo.R;
+import com.myhealthmemo.model.MealsItem;
 
 public class SettingMealAdapter extends ArrayAdapter<MealsItem> {
 	 private final Context context;
      private final ArrayList<MealsItem> mealsArrayList;
      
-     public SettingMealAdapter(Context context, ArrayList<MealsItem> settingsArrayList){
-    	 super(context, R.layout.target_item, settingsArrayList);
+     public SettingMealAdapter(Context context, ArrayList<MealsItem> mealsArrayList){
+    	 super(context, R.layout.target_item, mealsArrayList);
     	 this.context = context;
          this.mealsArrayList = mealsArrayList;
      }
@@ -27,17 +27,18 @@ public class SettingMealAdapter extends ArrayAdapter<MealsItem> {
     	 // 1. Create inflater 
          LayoutInflater inflater = (LayoutInflater) context
              .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+         // 2. Get rowView from inflater
+         View rowView = inflater.inflate(R.layout.target_item, parent, false);
+         // 3. Get icon,title & counter views from the rowView
+         ImageView imgView = (ImageView) rowView.findViewById(R.id.item_icon); 
+         TextView titleView = (TextView) rowView.findViewById(R.id.item_title);
+
+         // 4. Set the text for textView 
+         imgView.setImageResource(mealsArrayList.get(position).getIcon());
+         titleView.setText(mealsArrayList.get(position).getTitle());
+         
+         return rowView;
      }
      
-     // 2. Get rowView from inflater
-     
-     View rowView = null;
-     // 3. Get icon,title & counter views from the rowView
-     ImageView imgView = (ImageView) rowView.findViewById(R.id.item_icon); 
-     TextView titleView = (TextView) rowView.findViewById(R.id.item_title);
 
-     // 4. Set the text for textView 
-     imgView.setImageResource(mealsArrayList.get(position).getIcon());
-     titleView.setText(settingsArrayList.get(position).getTitle());
-  }
-}*/
+}
